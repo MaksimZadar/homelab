56f54b15879cf13d78eb0d46274a88cbe115d07a
MaksimZadar <maxzadarnovskiyy@gmail.com>
2025-04-05 22:24:52 +1030

    create better infra folder
    this will hold all the settings for vms / lxc containers as well as
    ansible play books for updating and installing extra software on these

---
+++ infra/docker/tofu/main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "3.0.1-rc3"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = var.az_resource_group_name
+    storage_account_name = var.az_storage_account_name
+    container_name       = var.az_container_name
+    key                  = var.az_tfstate_key
+  }
+}
+
+provider "proxmox" {
+  pm_api_url = var.pm_api_url
+  pm_api_token_id = var.pm_api_token_id
+  pm_api_token_secret = var.pm_api_token_secret
+  pm_tls_insecure = var.pm_tls_insecure
+}
+
+resource "proxmox_lxc" "docker_prod" {
+    hostname = var.docker_prod_hostname
+    target_node = var.docker_prod_proxmox_node
+    ostemplate   = var.docker_prod_os_template
+    description = var.docker_prod_lxc_description
+    password = var.docker_prod_password
+    unprivileged = true
+    tags = var.docker_prod_tags
+
+    ssh_public_keys = var.docker_prod_ssh_keys
+    
+    onboot = var.docker_prod_onboot
+    start = var.docker_prod_start
+
+    cores    = var.docker_prod_cpu_cores
+    memory   = var.docker_prod_memory
+    swap     = var.docker_prod_swap
+
+    network {
+      name   = var.docker_prod_network_name
+      bridge = var.docker_prod_network_bridge
+      ip     = var.docker_prod_network_ip
+      hwaddr = var.docker_prod_network_hwaddr
+    }
+
+    rootfs {
+      storage = var.docker_prod_disk_location
+      size    = var.docker_prod_disk_size
+    }
+
+    lifecycle {
+        ignore_changes = [
+            network,
+        ]
+    }
+}
+
+output "docker_lxc_ip" {
+  value = length(proxmox_lxc.docker_prod.network) > 0 ? proxmox_lxc.docker_prod.network[0].ip : "IP not available"
+  description = "The IP address of the Docker LXC container"
+}
---
+++ infra/docker/tofu/vars.tf
@@ -0,0 +1,108 @@
+# Proxmox Settings
+variable "pm_api_url" {
+  type = string
+}
+
+variable "pm_api_token_id" {
+  type = string
+}
+
+variable "pm_api_token_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "pm_tls_insecure" {
+  type = bool
+  description = "Set to true if the HTTPS certificate is self signed"
+}
+
+variable "docker_prod_proxmox_node" {
+  type = string
+}
+
+variable "docker_prod_disk_location" {
+  type = string
+}
+
+## Docker Prod VM Settings
+variable "docker_prod_hostname" {
+  type = string
+}
+
+variable "docker_prod_lxc_description" {
+  type = string
+}
+
+variable "docker_prod_tags" {
+  type = string
+}
+
+variable "docker_prod_os_template" {
+  type = string
+}
+
+variable "docker_prod_network_ip" {
+  type = string
+}
+
+variable "docker_prod_network_name" {
+  type = string
+}
+
+variable "docker_prod_network_bridge" {
+  type = string
+}
+
+variable "docker_prod_network_hwaddr" {
+  type = string
+}
+
+variable "docker_prod_onboot" {
+  type = bool
+}
+
+variable "docker_prod_start" {
+  type = bool
+}
+
+variable "docker_prod_disk_size" {
+  type = string
+}
+
+variable "docker_prod_memory" {
+  type = number
+}
+
+variable "docker_prod_swap" {
+  type = number
+}
+
+variable "docker_prod_cpu_cores" {
+  type = number
+}
+
+variable "docker_prod_password" {
+  type = string
+}
+
+variable "docker_prod_ssh_keys" {
+  type = string
+}
+
+# Azure backend settings
+variable "az_resource_group_name" {
+  type = string
+}
+
+variable "az_storage_account_name" {
+  type = string
+}
+
+variable "az_container_name" {
+  type = string
+}
+
+variable "az_tfstate_key" {
+  type = string
+}
